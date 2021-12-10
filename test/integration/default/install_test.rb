a5fa8827bf2414bf8299380841ff83c6dc12b04f
hieu <h3unb@gmail.com>
2021-12-11 06:57:24 +0700

    add tests

--- test/integration/default/install_test.rb
+++ test/integration/default/install_test.rb
@@ -6,13 +6,19 @@
 # found at http://inspec.io/docs/reference/resources/
 
 unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+  return 'Only supports Windows platform'
 end
 
+NETEXTENDER_VERSION = '10.2.313'
+
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('C:\Program Files (x86)\SonicWall\SSL-VPN\NetExtender\NECLI.exe') do
+  it { should exist }
+end
+
+# test NECLI version with call operator &
+#   Source: https://social.technet.microsoft.com/wiki/contents/articles/7703.powershell-running-executables.aspx#The_Call_Operator_amp
+describe command('& "C:\Program Files (x86)\SonicWall\SSL-VPN\NetExtender\NECLI.exe" -version') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match(/#{NETEXTENDER_VERSION}/) }
 end
