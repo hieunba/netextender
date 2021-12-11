b1f97ab477cce98aebdc04ac7c53e2d0c297964b
hieu <h3unb@gmail.com>
2021-12-11 07:34:06 +0700

    Update to install netExtender 10.2.315

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 
-default['netextender']['version'] = '10.2.313'
+default['netextender']['version'] = '10.2.315'
 default['netextender']['source'] = "https://software.sonicwall.com/NetExtender/NetExtender-#{node['netextender']['version']}.MSI"
